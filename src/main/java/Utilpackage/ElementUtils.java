648320a6cd899337735a886e49dbda544399d768
unknown <swetha.illan@gmail.com>
2020-02-18 21:55:21 +0000

    changed with apache poi

--- src/main/java/Utilpackage/ElementUtils.java
+++ src/main/java/Utilpackage/ElementUtils.java
@@ -1,10 +1,56 @@
 package Utilpackage;
 
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class ElementUtils {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ElementUtils extends DriverManager{
+
+    public static String TESTDATA_SHEET_PATH = "C:\\MyFirstProjects\\com.app.hubspot.com\\src\\main\\resources";
+
+   public  static Workbook book;
+    static Sheet sheet;
+    static JavascriptExecutor js;
+
+    public void switchToFrame() {
+        driver.switchTo().frame("mainpanel");
+    }
+
+    public static Object[][] getTestData(String sheetName) {
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(TESTDATA_SHEET_PATH);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            book = WorkbookFactory.create(file);
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sheet = book.getSheet(sheetName);
+        Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+        // System.out.println(sheet.getLastRowNum() + "--------" +
+        // sheet.getRow(0).getLastCellNum());
+        for (int i = 0; i < sheet.getLastRowNum(); i++) {
+            for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+                data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+                // System.out.println(data[i][k]);
+            }
+        }
+        return data;
+    }
 
 
    public void actionsToclickElement(){
